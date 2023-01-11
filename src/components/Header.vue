ef2d076b3ed40942fa9c843e32ec0011df5a9514
FlorianKomnino <florian.komnino@outlook.it>
2023-01-11 19:36:11 +0100

    added Yu-Gi-Oh img, started adding some styling rules

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,57 +11,20 @@ export default {
 <template>
     <div class="wrapper">
         <h1>
-            Header
+            yu-gi-oh api
         </h1>
     </div>
 </template>
 
 <style lang="scss" scoped>
-nav {
-    display: flex;
-}
-
-ul {
-    height: 100%;
-    display: flex;
-    align-items: center;
-}
-
-li {
-    height: 100%;
-    padding: 2rem 10px;
-    font-size: 0.9rem;
-    font-weight: 600;
-    text-transform: uppercase;
-    border-bottom: 2px solid transparent;
-
-    &:hover {
-        border-bottom: 2px solid #0282f9;
-        color: #0282f9;
-    }
-}
-
-img {
-    height: 80%;
-    margin: auto;
-}
-
-figure {
-    height: 100%;
-    display: flex;
-}
-
-div.primary-logo {
-    height: 70px;
-}
-
-.outerContainer {
+div.wrapper {
     width: 100%;
-    display: flex;
-    justify-content: space-between;
-}
+    height: 150px;
+    background-image: url('../assets/imgs/yu-gi-oh.jpeg');
 
-.innerContainer {
-    justify-content: space-between;
+    h1 {
+        text-transform: capitalize;
+        color: white;
+    }
 }
 </style>
