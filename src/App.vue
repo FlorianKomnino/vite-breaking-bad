ab7b3364a675c50da23b352b8967c0a17cba2055
FlorianKomnino <florian.komnino@outlook.it>
2023-01-13 21:52:08 +0100

    working selector of archetypes in yu-gi-oh api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <script>
 import { store } from './store.js';
 import axios from 'axios';
-import Main from './components/Main.vue'
 import Header from './components/Header.vue'
+import CardComponent from './components/cardComponent.vue'
 
 export default {
   components: {
-    Main,
     Header,
+    CardComponent,
   },
 
   data() {
     return {
       store,
+      apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
+      selectedArchetype: 'Alien',
     }
   },
 
   methods: {
     getCards() {
+
       //recupero le carte desiderate con questo metodo
-
-      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=Alien', {
+      axios.get(this.apiUrl, {
         params: {
-
+          archetype: this.store.chosenArchetype,
+          num: 10,
+          offset: 0,
         }
       })
         .then((response) => {
@@ -40,7 +44,6 @@ export default {
   },
 
   created() {
-    this.getCards();
   }
 }
 </script>
@@ -51,7 +54,47 @@ export default {
   </header>
 
   <main>
-    <Main />
+    <div class="container">
+
+      <!-- selector -->
+      <div class="row" id="selectorRow">
+        <div class="col-12">
+          <div class="selectorWrapper">
+            <label for="archetypeSelector">Choose the archetype you want to see...</label>
+
+
+            <select @change="getCards" name="archetypes" id="archetypeSelector" v-model="store.chosenArchetype">
+
+              <option v-for="(archetype, index) in store.archetypesList" :value="archetype">
+                {{ archetype }}
+              </option>
+
+            </select>
+
+
+          </div>
+        </div>
+      </div>
+
+      <!-- cards found number -->
+      <div class="row">
+        <div class="col-12">
+          <h1>
+            Found {{ store.arrayOfFoundCardsLength }} cards
+          </h1>
+        </div>
+      </div>
+
+      <!-- cards container -->
+      <div class="row">
+        <div class="cardsContainer col-12">
+          <CardComponent v-for="card, index in store.cardsArrayFromResponse"
+            :imgPath="store.cardsArrayFromResponse[index].card_images[0].image_url"
+            :cardName="store.cardsArrayFromResponse[index].name"
+            :archetype="store.cardsArrayFromResponse[index].archetype" :key="store.chosenArchetype" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
@@ -60,5 +103,31 @@ export default {
 
 main {
   background-color: #de8522;
+}
+
+h1 {
+  background-color: white;
+  padding: 2rem 0 0 2rem;
+}
+
+div.selectorWrapper {
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+  color: white;
+  font-size: 1.1rem;
+  font-weight: 600;
+
+  select {
+    width: 150px;
+  }
+}
+
+div.cardsContainer {
+  width: calc(100% - 1.5rem);
+  background-color: white;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -4,4 +4,5 @@ export const store = reactive({
     cardsArrayFromResponse: {},
     arrayOfFoundCardsLength: 0,
     archetypesList: ['Alien', 'Laval', 'Vylon', 'Inzektor', 'Umi', 'Gusto'],
+    chosenArchetype: '',
 })
